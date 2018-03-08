6752f15212948f0e1882db6359027639a13b9ff7
jayzen <jayzen@foxmail.com>
2018-03-08 11:14:48 +0800

    add binary search

---
+++ binary_search.rb
@@ -0,0 +1,29 @@
+# Algorithm
+class Array
+  def binary_search(value)
+    low = 0
+    high = self.size - 1
+
+    while(low <= high) do
+      mid = low + (high - low) / 2
+
+      if self[mid] == value
+        return mid
+      elsif self[mid] < value
+        low = mid + 1
+      else
+        high = mid - 1
+      end
+    end
+    return nil 
+  end
+end
+
+#Realization
+p (1..100).to_a.binary_search(100) #99
+p (1..100).to_a.binary_search(199) #nil
+
+
+#Terminology
+#iteration(迭代) : update the value of the variables
+#recursion(递归) : call self in the inner function
---
+++ readme.rb
@@ -0,0 +1,1 @@
+Algorithm Realization in Ruby Version
